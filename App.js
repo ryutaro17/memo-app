07dde51285338dde53fa58fcf1057c481d26be41
Ryutaro Hayashi <ryutaro@transfield.co.jp>
2020-02-10 23:34:45 +0900

    Create MemoEdit, Login, Signup screen.

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { StyleSheet, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
 //import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoListScreen from './src/screens/MemoListScreen';
+//import MemoListScreen from './src/screens/MemoListScreen';
+//import MemoEditScreen from './src/screens/MemoEditScreen';
+//import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <AppBar />
-        <MemoListScreen />
+        <SignupScreen />
       </View>
     );
   }
@@ -21,7 +24,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFDF6',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: 78,
   },
 });
---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.memoEditInput} multiline value="Hi" />
+        <CircleButton>
+          {'\uf00c'}
+        </CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    backgroundColor: '#ddd',
+    fontSize: 20,
+  },
+});
+
+export default MemoEditScreen;
